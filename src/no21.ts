85c2348b03a9930f1b403dedda2fe09316772702
Redstone Zhao <13740080@qq.com>
2021-10-02 15:24:33 +0800

    完成21题

---
+++ src/no21.ts
@@ -0,0 +1,17 @@
+// @link https://github.com/semlinker/awesome-typescript/issues/40
+// 实现一个 Merge 工具类型，用于把两个类型合并成一个新的类型。第二种类型（SecondType）的 Keys 将会覆盖第一种类型（FirstType）的 Keys。具体的使用示例如下所示：
+
+type Foo = {
+   a: number;
+   b: string;
+};
+
+type Bar = {
+   b: number;
+};
+
+type Simplify<T> = { [k in keyof T]: T[k]; }
+type Merge<FirstType, SecondType> = Simplify<Omit<FirstType, keyof SecondType> & SecondType>;
+
+type T1 = Merge<Foo, Bar>
+const ab: Merge<Foo, Bar> = { a: 1, b: 2 };
